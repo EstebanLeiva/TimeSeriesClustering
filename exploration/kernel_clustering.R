7b2a4b6ab8ee174d3894936be45566b672b0a594
EstebanLeiva <e.leivam@uniandes.edu.co>
2024-07-09 11:12:37 -0700

    Kernel clustering with time windows and return time series

--- exploration/kernel_clustering.R
+++ exploration/kernel_clustering.R
@@ -3,7 +3,7 @@ library(dtw)
 library(cluster)
 library(np)
 
-smooth_series <- function(time_series, bw, kernel = "normal") {
+smooth_series <- function(time_series, bw, kernel) {
   smooth_series <- time_series  
   for (col in names(time_series)[1:ncol(smooth_series)]) {
     X <- 1:nrow(time_series)
@@ -35,13 +35,13 @@ diss_matrix <- function(data, distance) {
       }
     }
   }
-  return(diss)
+  return(as.dist(diss))
 }
 
-kernel_clustering <- function(time_series, k, bw, kernel = "normal", distance = "euclidean") {
+kernel_clustering <- function(time_series, k, bw, kernel, distance) {
   smoothed_series <- smooth_series(time_series, bw, kernel)
   diss <- diss_matrix(smoothed_series, distance)
-  cluster <- pam(diss, k = k)
+  cluster <- pam(diss, k)
   return(cluster)
 }
 
@@ -49,20 +49,21 @@ kernel_clustering_time_window <- function(time_series, k, bw, kernel = "normal",
   if (!is.null(window_length)){
     n <- nrow(time_series)
     boundaries <- seq(1, n, by = window_length)
-    clusters <- c()
+    clusters <- matrix(NA, nrow = length(boundaries) - 1, ncol = ncol(time_series))
     for (i in 1:(length(boundaries) - 1)){
       window <- time_series[boundaries[i]:boundaries[i + 1], ]
-      cluster <- kernel_clustering(smooth_window, k, bw, kernel, distance)
-      clusters <- c(clusters, cluster$clustering)
+      cluster <- kernel_clustering(window, k, bw, kernel, distance)
+      clusters[i, ] <- cluster$clustering
     }
     return(clusters)
   } else if (!is.null(change_points)){
-    clusters <- c()
-    for (i in 1:(length(change_points) - 1)){
-      window <- time_series[change_points[i]:change_points[i + 1], ]
-      cluster <- kernel_clustering(smooth_window, k, bw, kernel, distance)
-      clusters <- c(clusters, cluster$clustering)
-    }
+      clusters <- matrix(NA, nrow = length(change_points) - 1, ncol = ncol(time_series))
+      for (i in 1:(length(change_points) - 1)){
+        window <- time_series[change_points[i]:change_points[i + 1], ]
+        cluster <- kernel_clustering(window, k, bw, kernel, distance)
+        clusters[i, ] <- cluster$clustering
+      }
+      return(clusters)
   } else {
     print("Window length or change points must be provided")
   }
@@ -93,19 +94,57 @@ lines(X, smoothed$y, col = "red")
 lines(time.seq$X, reg.eval$mean, col = "red")
 
 ### Clustering example ###
-cluster <- kernel_ts_clustering(time_series, 3)
+time_series <- read.csv("exploration/data/combined_brownian_database.csv")
+cluster <- kernel_clustering(time_series, 3, 3, "normal", "dtw")
 
 clusters <- cluster$clustering
+clusters
 for (i in 1:3){
-    plot(NULL, xlim = c(0, nrow(smoothed_series)), ylim = c(-5, 5), xlab = "Time", ylab = "Daily Return", main = "Cluster")
-    for (j in 1:ncol(smoothed_series)){
+    plot(NULL, xlim = c(0, nrow(time_series)), ylim = c(-5, 5), xlab = "Time", ylab = "Daily Return", main = "Cluster")
+    for (j in 1:ncol(time_series)){
         if (clusters[j] == i){
-            lines(smoothed_series[,j+1], type = "l", col = i)
+            lines(time_series[,j+1], type = "l", col = i)
         }
     }
 }
 
-#dtw
-#simulated stocks with increases and decreases, and get daily returns
-#real stocks returns and get daily returns
-#time windows in change points
+### Time window clustering example ###
+time_series <- read.csv("exploration/data/combined_brownian_database.csv")
+change_points <- c(1, 15, 30)
+clusters <- kernel_clustering_time_window(time_series, 3, 3, "normal", "dtw", change_points = change_points)
+clusters[1, ]
+clusters[2, ]
+
+### Plot Return Series ###
+return_series <- read.csv("exploration/data/browniancombined_returns.csv")
+smooth_returns <- smooth_series(return_series, 3, "normal")
+plot(return_series$BM_1, type = "l", xlab = "Time", ylab = "Daily Return", main = "Return Series")
+lines(smooth_returns$BM_1, col = "red")
+
+### Return Clustering ###
+return_series <- read.csv("exploration/data/browniancombined_returns.csv")
+cluster <- kernel_clustering(return_series, 3, 3, "normal", "dtw")
+clusters <- cluster$clustering
+clusters
+
+smooth_returns <- smooth_series(return_series, 3, "normal")
+
+for (i in 1:3){
+    plot(NULL, xlim = c(0, nrow(return_series)), ylim = c(-1, 1), xlab = "Time", ylab = "Daily Return", main = "Cluster")
+    for (j in 1:ncol(return_series)){
+      if (clusters[j] == i){
+        points(return_series[, j], type = "p", col = i, pch = 19)        
+        lines(smooth_returns[, j], type = "l", col = "grey")
+      }
+    }
+}
+
+plot(NULL, xlim = c(0, nrow(return_series)), ylim = c(-1, 1), xlab = "Time", ylab = "Daily Return", main = "Cluster")
+for (i in 1:3){
+  for (j in 1:ncol(return_series)){
+    if (clusters[j] == i){
+      lines(smooth_returns[, j], type = "l", col = i)
+    }
+  }
+}
+
